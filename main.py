91e950be8ba48e6aed99539329e00c94291592cd
Andre Tischenko <39458631+Fuegos@users.noreply.github.com>
2022-06-03 10:01:52 +0400

    upd

--- main.py
+++ main.py
@@ -10,18 +10,59 @@ def writeFile(path, data):
     file.writelines(data)
     file.close()
 
+def prepareForWrite(list):
+    return [str + "\n" for str in list]
 
-def findNewList(listOne, listTwo):
-    return [str + "\n" for str in listOne if str not in listTwo]
+
+def findUnique(rangeOne, rangeTwo):
+    return [str for str in rangeOne if str not in rangeTwo]
 
 
-listOne = readFile("./input/1.txt")
-listTwo = readFile("./input/2.txt")
-listOne.sort()
-listTwo.sort()
-writeFile("./output/1.txt", findNewList(listOne, listTwo))
-writeFile("./output/2.txt", findNewList(listTwo, listOne))
+# Create dictonary with first string's symbol and his area
+def createMask(file):
+    mask = {}
+    for index, str in enumerate(file):
+        if str[0] in mask:
+            mask[str[0]]["end"] = index + 1
+        else:
+            mask[str[0]] = {"start": index, "end": index + 1}
+    return mask
 
+
+def getRangeByMask(key, file, mask):
+    return file[mask[key]["start"]:mask[key]["end"]]
+
+
+def findList(fileOne, maskOne, fileTwo, maskTwo):
+    result = []
+    for key in maskOne:
+        if key in maskTwo:
+            result.extend(
+                findUnique(
+                    getRangeByMask(key, fileOne, maskOne),
+                    getRangeByMask(key, fileTwo, maskTwo)
+                )
+            )
+        else:
+            result.extend(
+                getRangeByMask(key, fileOne, maskOne)
+            )
+    return result
+
+
+
+fileOne = readFile("./input/1.txt")
+fileTwo = readFile("./input/2.txt")
+
+maskOne = createMask(fileOne)
+maskTwo = createMask(fileTwo)
+
+listOne = findList(fileOne, maskOne, fileTwo, maskTwo)
+listTwo = findList(fileTwo, maskTwo, fileOne, maskOne)
+
+
+writeFile("./output/1.txt", prepareForWrite(listOne))
+writeFile("./output/2.txt", prepareForWrite(listTwo))
 
 # writeFile("./input/1.txt", [str + "\n" for str in listOne])
 # writeFile("./input/2.txt", [str + "\n" for str in listTwo])
